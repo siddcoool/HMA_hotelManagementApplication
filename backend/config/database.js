149d7b49916b8202883d11becfb9a3e2e64b1b5d
Saurabh Tripathi <71284408+siddcoool@users.noreply.github.com>
2023-11-03 17:23:24 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,8 +6,8 @@ const mongoURI = process.env.MONGODB_URL
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    redisConnection.connect()
-      .then(() => console.log('Redis Connected!'))
+   // redisConnection.connect()
+   //   .then(() => console.log('Redis Connected!'))
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
